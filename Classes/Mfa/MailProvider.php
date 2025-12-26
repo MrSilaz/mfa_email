10c224f5eb271498935e1ae30dd09cb0791abac2
MrSilaz <ralf@freit.de>
2025-12-26 11:48:48 +0100

    Set version to 1.0.6 and improve handling for uninitialized auth codes.

--- Classes/Mfa/MailProvider.php
+++ Classes/Mfa/MailProvider.php
@@ -136,7 +136,7 @@ class MailProvider implements MfaProviderInterface
             return false;
         }
 
-        $properties['authCode'] = '';
+        $properties['authCode'] = 'unset_'.$this->generateAuthCode();
         $properties['attempts'] = 0;
         $properties['lastUsed'] = $this->context->getPropertyFromAspect('date', 'timestamp');
 
@@ -221,7 +221,8 @@ class MailProvider implements MfaProviderInterface
     {
         $newAuthCode = false;
         $authCode = $propertyManager->getProperty('authCode');
-        if (empty($authCode)) {
+
+        if (empty($authCode) || substr($authCode, 0, 6) === 'unset_') {
             $authCode = $this->generateAuthCode();
             $propertyManager->updateProperties(['authCode' => $authCode]);
             $newAuthCode = true;
