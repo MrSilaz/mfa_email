62e5ca748b5255ba837794fa4639f3547d825486
MrSilaz <ralf@freit.de>
2026-03-18 17:27:15 +0100

    Quick fix: removed unset_ again
    (cherry picked from commit c9176ab4d8b812748d12d0dfa5cf8fb4a79caed1)

--- Classes/Mfa/MailProvider.php
+++ Classes/Mfa/MailProvider.php
@@ -148,7 +148,7 @@ class MailProvider implements MfaProviderInterface
             return false;
         }
 
-        $properties['authCode'] = 'unset_'.$this->generateAuthCode();
+        $properties['authCode'] = '';
         $properties['attempts'] = 0;
         $properties['lastUsed'] = $this->context->getPropertyFromAspect('date', 'timestamp');
 
@@ -235,8 +235,7 @@ class MailProvider implements MfaProviderInterface
     {
         $newAuthCode = false;
         $authCode = $propertyManager->getProperty('authCode');
-
-        if (empty($authCode) || substr($authCode, 0, 6) === 'unset_') {
+        if (empty($authCode)) {
             $authCode = $this->generateAuthCode();
             $propertyManager->updateProperties(['authCode' => $authCode]);
             $newAuthCode = true;
