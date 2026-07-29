e6fd7037b76bc79356cf60ff84a49e25f489b6ec
Benno Nijholt <nijholt.benno@windinternet.nl>
2026-07-29 11:21:24 +0200

    [TASK] Add support for TYPO3 V14.3
    Includes security hardening changes to the MailProvider in addition to needed API changes to work with TYPO3 V14.3
    Also added default values to prevent errors if user were to enter incompatible values
    Dropped support for v13.4. ext_emconf.php is deprecated since v14.2, consider updating your tooling so it can be removed in the future

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * Since TYPO3 v14.2 this file is no longer evaluated.
+ * all metadata is read from composer.json (see "extra/typo3/cms").
+ * It is only kept in sync for third party tooling such as the TYPO3 Extension Repository and Tailor
+ */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'E-Mail MFA Provider',
     'description' => 'Provides a multi-factor authentication via E-Mail for TYPO3',
@@ -8,10 +13,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'ralf@freit.de',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '14.3.0-14.3.99',
+            'php' => '8.2.0-8.5.99',
         ],
         'conflicts' => [
         ],
